bbf52048cc72a83d924ea38efc75abfa71fcc17e
Kris McCann <kris@8pi.ca>
2020-09-05 19:48:47 +0100

    initial commit.

---
+++ lotto.js
@@ -0,0 +1,85 @@
+(() => {
+    const low = process.argv[2] || 1,
+        high = process.argv[3] || 59,
+        numPicks = process.argv[4] || 6,
+        numDraws = process.argv[5] || 1000000000;
+
+    const lotto = (low, high, numPicks, numDraws) => {
+        const results = [high],
+            displayResults = [];
+        
+        console.log('\nLow ball: ' + low, '\nHigh ball: ' + high, '\nNumber of picks: ' + numPicks, '\nNumber of draws: ' + numDraws);
+    
+        for (let count = 0; count < high; count++) {
+            results[count] = {ball: count + parseInt(low), score: 0};
+        }
+    
+        for (let count = 0; count < numDraws; count++) {
+            draw = Math.round(Math.random() * (high - low));
+            results[draw].score++;
+        }
+            
+        results.sort((a, b) => b.score - a.score);
+    
+        for (let count = 0; count < numPicks; count++) {
+            displayResults.push(results[count]);
+        }
+            
+        displayResults.sort((a, b) => a.ball - b.ball);
+    
+        console.log(displayResults);
+    };
+    
+    const showHelp = () => {
+        console.log(
+            'Usage: node lotto.js {low ball} {high ball} {number of picks} {number of draws}\n',
+            'Defaults: {low ball: 1} {high ball: 50} {number of picks: 5} {number of draws: 1000000000}'
+        );
+    };
+    
+    const inputError = () => {
+        let valid = true;
+        for (let count = 2; count < process.argv.length; count++) {
+            if(parseInt(process.argv[count]) < 0) {
+                valid = false;
+                break;
+            }
+            if (isNaN (parseInt(process.argv[count]))) {
+                valid = false;
+                break;
+            }
+        }
+        if (valid && (parseInt(process.argv[2]) > parseInt(process.argv[3]))) {
+            valid = false;
+            console.log('Low ball cannot be higher than high ball')
+        }
+        return !valid;
+    };
+    
+    const formatTime = (duration) => {
+        let milliseconds = parseInt((duration % 1000)),
+            seconds = Math.floor((duration / 1000) % 60),
+            minutes = Math.floor((duration / (1000 * 60)) % 60),
+            hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+            
+            console.log('dur' + duration);
+            
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+        return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+    };
+    
+    if (process.argv[2] == '--help' || process.argv[2] == '--h' || process.argv[2] == '-?') {
+        showHelp();
+    } else {
+        if (inputError()) {
+            showHelp();
+        } else {
+            const startTime = new Date().getTime();
+            lotto(low, high, numPicks, numDraws);
+            const endTime = new Date().getTime();
+            console.log('Execution time: ' + formatTime(parseInt(endTime) - parseInt(startTime)));
+        }
+    }
+})();
