9e04349841e592b028d7826bc5d53cb99a258292
Kris McCann <kris@8pi.ca>
2020-09-09 03:47:06 +0100

    move to full es6 module

--- lotto.js
+++ lotto.js
@@ -1,99 +1,96 @@
-const notifier = require('node-notifier');
+import notifier from 'node-notifier';
 
-(() => {
-    const lotto = (low, high, numPicks, numDraws, drawType) => {
-        const results = [high],
-        displayResults = [],
-        accumFactor = 10,
-        lastNum = {
-            num: 0,
-            score: 1
-        };
+const lotto = (low, high, numPicks, numDraws, drawType) => {
+    const results = [high],
+    displayResults = [],
+    accumFactor = 100,
+    lastNum = {
+        num: 0,
+        score: 1
+    };
 
-        console.log(`\n${(drawType == 'accum' ? 'Accumulator' : 'Draw')}\nLow ball: ${low}\nHigh ball: ${high}\nNumber of picks: ${numPicks}\nNumber of draws: ${numDraws}`);
+    let draw;
 
-        for (let count = 0; count < high; count++) {
-            results[count] = {ball: count + parseInt(low), score: 0};
-        }
+    console.log(`\n${(drawType == 'accum' ? 'Accumulator' : 'Draw')}\nLow ball: ${low}\nHigh ball: ${high}\nNumber of picks: ${numPicks}\nNumber of draws: ${numDraws}`);
 
-        if (drawType == 'accum') {
-            for (let count = 0; count < numDraws; count++) {
-                draw = Math.round(Math.random() * (high - low));
-                if (draw == lastNum.num) {
-                    results[draw].score += lastNum.score;
-                    lastNum.score *= accumFactor;
-                } else {
-                    lastNum.num = draw;
-                    lastNum.score = 1;
-                }
-            }
-        } else {
-            for (let count = 0; count < numDraws; count++) {
-                draw = Math.round(Math.random() * (high - low));
-                results[draw].score++;
-            }
-        }
-
-        results.sort((a, b) => b.score - a.score);
-
-        for (let count = 0; count < numPicks; count++) {
-            displayResults.push(results[count]);
-        }
-
-        displayResults.sort((a, b) => a.ball - b.ball);
-
-        console.log(displayResults);
+    for (let count = 0; count < high; count++) {
+        results[count] = {ball: count + parseInt(low), score: 0};
     }
 
-    const showHelp = () => {
-        console.log(
-            'Usage: node lotto.js {low ball} {high ball} {number of picks} {number of draws} {drawType: draw|accum}\n',
-            'Defaults: {low ball: 1} {high ball: 59} {number of picks: 6} {number of draws: 1000000000} {draw}'
-        );
-    };
-
-    const inputError = () => {
-        let valid = true;
-        for (let count = 2; count < 5; count++) {
-            if(parseInt(process.argv[count]) < 0) {
-                valid = false;
-                break;
-            }
-            if (isNaN (parseInt(process.argv[count]))) {
-                valid = false;
-                break;
+    if (drawType == 'accum') {
+        for (let count = 0; count < numDraws; count++) {
+            draw = Math.round(Math.random() * (high - low));
+            if (draw == lastNum.num) {
+                results[draw].score += lastNum.score;
+                lastNum.score *= accumFactor;
+            } else {
+                lastNum.num = draw;
+                lastNum.score = 1;
             }
         }
-        if (valid && (parseInt(process.argv[2]) > parseInt(process.argv[3]))) {
-            valid = false;
-            console.log('Low ball cannot be higher than high ball')
+    } else {
+        for (let count = 0; count < numDraws; count++) {
+            draw = Math.round(Math.random() * (high - low));
+            results[draw].score++;
         }
-        return !valid;
-    };
+    }
 
-    const formatTime = (duration) => {
-        let milliseconds = parseInt((duration % 1000)),
-            seconds = Math.floor((duration / 1000) % 60),
-            minutes = Math.floor((duration / (1000 * 60)) % 60),
-            hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    results.sort((a, b) => b.score - a.score);
 
-        hours = (hours < 10) ? "0" + hours : hours;
-        minutes = (minutes < 10) ? "0" + minutes : minutes;
-        seconds = (seconds < 10) ? "0" + seconds : seconds;
-        return `${hours}:${minutes}:${seconds}.${milliseconds}`;
-    };
+    for (let count = 0; count < numPicks; count++) {
+        displayResults.push(results[count]);
+    }
+
+    displayResults.sort((a, b) => a.ball - b.ball);
+
+    console.log(displayResults);
+};
+
+const showHelp = () => {
+    console.log(
+        'Usage: node lotto.js {low ball} {high ball} {number of picks} {number of draws} {drawType: draw|accum}\n',
+        'Defaults: {low ball: 1} {high ball: 59} {number of picks: 6} {number of draws: 1000000000} {draw}'
+    );
+};
+
+const inputError = (low, high, numPicks, numDraws, drawType) => {
+    if(parseInt(low) < 0 ||
+        parseInt(high) < 0 ||
+        parseInt(numPicks) < 0 ||
+        parseInt(numDraws) < 0
+    ) {
+        return true;
+    }
+    if (parseInt(low) > parseInt(high)) {
+        console.log('Low ball cannot be higher than high ball');
+        return true;
+    }
+    return false;
+};
+
+const formatTime = (duration) => {
+    let milliseconds = parseInt((duration % 1000)),
+        seconds = Math.floor((duration / 1000) % 60),
+        minutes = Math.floor((duration / (1000 * 60)) % 60),
+        hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    return `${hours}:${minutes}:${seconds}.${milliseconds}`;
+};
 
 
+const runLotto = () => {
     const low = process.argv[2] || 1,
         high = process.argv[3] || 59,
         numPicks = process.argv[4] || 6,
-        numDraws = process.argv[5] || 1000000000; // takes about 16 secs per 1000000000 on draw or 1:07 accumulator
+        numDraws = process.argv[5] || 1000000000, // takes about 16 secs per 1000000000 on draw or 1:07 accumulator
         drawType = process.argv[6] || 'draw';
-
-    if (process.argv[2] == '--help' || process.argv[2] == '--h' || process.argv[2] == '-?') {
+    if (process.argv[2] == '--help' || process.argv[2] == '--h' || process.argv[2] == '-h' || process.argv[2] == '-?') {
         showHelp();
     } else {
-        if (inputError()) {
+        if (inputError(low, high, numPicks, numDraws, drawType)) {
             notifier.notify(
                 {
                     title: 'Error',
@@ -122,4 +119,10 @@ const notifier = require('node-notifier');
             console.log(`Execution time: ${endTime}`);
         }
     }
-})();
+};
+
+runLotto();
+
+export default {
+    runLotto
+}
