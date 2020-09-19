2e9cf4741d61234559a550419a019729275235d6
Kris McCann <kris@8pi.ca>
2020-09-19 14:45:40 +0100

    added mincon mode

--- lotto.js
+++ lotto.js
@@ -1,61 +1,102 @@
 import notifier from 'node-notifier';
 
-const runLotto = (low, high, numPicks, numDraws, drawType) => {
+const runLotto = (low, high, numPicks, numDraws, drawType, minConsecutive) => {
     const results = [high],
     displayResults = [],
     accumFactor = 10,
     lastNum = {
         num: 0,
-        score: 1,
         curInRow: 1
     };
 
     let draw,
+        drawsMet = 0,
         drawTypeText = "Draw";
 
     switch (drawType) {
         case 'accum':
             drawTypeText = "Accumulator";
             break;
-        case 'accummax':
+        case 'accumax':
             drawTypeText = "Max Accumulator";
+            break;
+        case 'mincon':
+            drawTypeText = "Minimum Consecutive";
             break;
     }
 
-    console.log(`\n${drawTypeText}\nLow ball: ${low}\nHigh ball: ${high}\nNumber of picks: ${numPicks}\nNumber of draws: ${numDraws}`);
-
     for (let count = 0; count < high; count++) {
-        results[count] = {ball: count + parseInt(low), score: 0};
-        if (drawType == 'accummax') {
+        results[count] = {ball: count + parseInt(low)};
+        if (drawType != 'mincon') {
+            results[count].score = 0;
+        }
+        if (drawType == 'accumax' || drawType == 'mincon') {
             results[count].maxInRow = 1;
         }
     }
 
-    for (let count = 0; count < numDraws; count++) {
-        draw = Math.round(Math.random() * (high - low));
-        if (drawType == 'accum' || drawType == 'accummax') {
-            if (draw == lastNum.num) {
-                results[draw].score += lastNum.score;
-                lastNum.score *= accumFactor;
-                if (drawType == 'accummax') {
-                    lastNum.curInRow ++;
-                    if (results[draw].maxInRow < lastNum.curInRow) {
-                        results[draw].maxInRow = lastNum.curInRow;
-                    }
-                }
-            } else {
-                lastNum.curInRow = 1;
-                lastNum.num = draw;
-                lastNum.score = 1;
-            }
-        } else {
-            results[draw].score++;
-        }
+    if (drawType != 'mincon') {
+        console.log(`\n${drawTypeText}\nLow ball: ${low}\nHigh ball: ${high}\nNumber of picks: ${numPicks}\nNumber of draws: ${numDraws}`);
+    } else {
+        console.log(`\n${drawTypeText}\nLow ball: ${low}\nHigh ball: ${high}\nNumber of picks: ${numPicks}\nMin consecutive draws: ${minConsecutive}`);
     }
+
+
+    switch (drawType) {
+        case 'draw':
+            for (let count = 0; count < numDraws; count++) {
+                draw = Math.round(Math.random() * (high - low));
+                results[draw].score++;
+            }
+            break;
+        case 'accum':
+        case 'accumax':
+            for (let count = 0; count < numDraws; count++) {
+                draw = Math.round(Math.random() * (high - low));
+                if (draw == lastNum.num) {
+                    results[draw].score += lastNum.score;
+                    lastNum.score *= accumFactor;
+                    if (drawType == 'accumax') {
+                        lastNum.curInRow ++;
+                        if (results[draw].maxInRow < lastNum.curInRow) {
+                            results[draw].maxInRow = lastNum.curInRow;
+                        }
+                    }
+                } else {
+                    lastNum.curInRow = 1;
+                    lastNum.num = draw;
+                    lastNum.score = 1;
+                }
+            }
+            break;
+        case 'mincon':
+            lastNum.curInRow = 1;
+            while (drawsMet < numPicks) {
+                draw = Math.round(Math.random() * (high - low));
+                if (draw == lastNum.num) {
+                    if (results[draw].maxInRow < minConsecutive) {
+                        lastNum.curInRow++;
+                        results[draw].maxInRow = lastNum.curInRow;
+                        if (results[draw].maxInRow == minConsecutive) {
+                            drawsMet++;
+                            lastNum.curInRow = 1;
+                        }
+                    }
+                } else {
+                    lastNum.num = draw;
+                    lastNum.curInRow = 1;
+                }
+            }
+            break;
+        }
 
     results.sort((a, b) => b.score - a.score);
 
-    if (drawType == 'accummax') {
+    if (drawType == 'accumax') {
+        results.sort((a, b) => b.maxInRow - a.maxInRow);
+    }
+
+    if (drawType == 'mincon') {
         results.sort((a, b) => b.maxInRow - a.maxInRow);
     }
 
@@ -63,23 +104,31 @@ const runLotto = (low, high, numPicks, numDraws, drawType) => {
         displayResults.push(results[count]);
     }
 
-    displayResults.sort((a, b) => a.ball - b.ball);
+    if (drawType != 'mincon') {
+        displayResults.sort((a, b) => a.ball - b.ball);
+    }
 
     console.log(displayResults);
 };
 
 const showHelp = () => {
     console.log(
-        'Usage: node lotto.js {low ball} {high ball} {number of picks} {number of draws} {drawType: draw|accum|accummax}\n',
-        'Defaults: {low ball: 1} {high ball: 59} {number of picks: 6} {number of draws: 1000000000} {draw}'
+        '\nUsage: node lotto.js {low ball} {high ball} {number of picks} {number of draws} {drawType: draw|accum|accumax|mincon} {min consecutive picks}\n',
+        '\nDefaults: {low ball: 1} {high ball: 59} {number of picks: 6} {number of draws: 1000000000} {draw}',
+        '\nDraw (draw) mode just picks a number and increments the score of the associated ball each time it is picked',
+        '\nAccumulator (accum) mode applies a multiplier to consecutive picks, giving a higher score to balls that show up multipled times in a row',
+        '\nMax Accumulator (accumax) mode is the same as accumulator, but ranks results by concurrent draws, not by score',
+        '\nIn Minimum Consecutive (mincon) mode number of draws is irrelevant, as draws will continue until the desired number of picks has been met in concurrent draws',
+        '\nIn other draw modes, min consecutive picks is not a required param, as the draws will continue until the specified number of draws'
     );
 };
 
-const inputError = (low, high, numPicks, numDraws, drawType) => {
+const inputError = (low, high, numPicks, numDraws, drawType, minConsecutive) => {
     if(parseInt(low) < 0 ||
         parseInt(high) < 0 ||
         parseInt(numPicks) < 0 ||
-        parseInt(numDraws) < 0
+        parseInt(numDraws) < 0 ||
+        parseInt(minConsecutive) < 0
     ) {
         return true;
     }
@@ -108,7 +157,8 @@ const lotto = () => {
         high = process.argv[3] || 59,
         numPicks = process.argv[4] || 6,
         numDraws = process.argv[5] || 1000000000, // takes about 16 secs per 1000000000 on draw or 1:07 accumulator
-        drawType = process.argv[6] || 'draw';
+        drawType = process.argv[6] || 'draw',
+        minConsecutive = process.argv[7] || 5; // minimum consecutive draws in mincon mode
     if (process.argv[2] == '--help' || process.argv[2] == '--h' || process.argv[2] == '-h' || process.argv[2] == '-?') {
         showHelp();
     } else {
@@ -125,7 +175,7 @@ const lotto = () => {
             showHelp();
         } else {
             const startTime = new Date().getTime(),
-                results = runLotto(low, high, numPicks, numDraws, drawType),
+                results = runLotto(low, high, numPicks, numDraws, drawType, minConsecutive),
                 endTime = formatTime(parseInt(new Date().getTime()) - parseInt(startTime));
 
             notifier.notify(
